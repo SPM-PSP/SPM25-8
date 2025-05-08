14f679913be8e6e056d29cc64c8ed661d0ce9101
Famarey <3414149763@qq.com>
2025-05-08 18:51:00 +0800

    测试

---
+++ 源代码/personal-health-api/src/test/java/cn/kmbeast/context/LocalThreadHolderTest.java
@@ -0,0 +1,24 @@
+package cn.kmbeast.context;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalThreadHolderTest {
+
+    @Test
+    void setUserId() {
+    }
+
+    @Test
+    void getUserId() {
+    }
+
+    @Test
+    void getRoleId() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
